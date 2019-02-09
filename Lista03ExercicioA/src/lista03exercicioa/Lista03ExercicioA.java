67dd38fb54aa52ab081ccf3c4bdb883a5f5023af
JP-Garcia01 <xx.jpgarcia3.xx@gmail.com>
2019-02-08 22:22:06 -0200

    Aula 1- Exercicios Temperatura

---
+++ Lista03ExercicioA/src/lista03exercicioa/Lista03ExercicioA.java
@@ -0,0 +1,63 @@
+package lista03exercicioa;
+import javax.swing.JOptionPane;
+import static javax.swing.JOptionPane.*;
+public class Lista03ExercicioA {
+    static final String NOME_APP = "CONVERTER 5000"; //static final o valor nao pode ser mais alterado
+
+    public static void main(String[] args) {
+        int opcao = lerInt(
+                "Qual opção abaixo você deseja de conversão?\n"+
+                "1 - Celsius para Fahrenheit\n" + 
+                "2 - Fahrenheit para Celsius\n"
+        );
+        
+        double graus = lerDouble(
+                "Digite temperatura desejada"
+        );
+        
+        double conversao = 0.0;
+        
+        switch(opcao){
+            case 1:
+             conversao = (9 * graus + 160)/5;
+             break;
+             
+            case 2:
+             conversao = ((graus - 32) * 5) / 9;
+             
+             default:
+             conversao = Double.MAX_VALUE;
+        };
+        
+        exibir(
+                "O valor digitado é " + (conversao == Double.MAX_VALUE ? "[ERRO]" : "" + conversao)
+        );
+        
+    }// main
+        public static String ler(Object mensagem){
+        return showInputDialog(
+                null, //janela pai
+                mensagem, //mensagem
+                NOME_APP, //titulo
+                JOptionPane.PLAIN_MESSAGE 
+            );
+        }//ler
+        
+        public static double lerDouble(Object mensagem){
+            return Double.parseDouble(ler(mensagem));
+        }//lerDouble
+        
+        public static int lerInt(Object mensagem){
+            return Integer.parseInt(ler(mensagem));
+        }//lerInt
+        
+       public static void exibir(Object mensagem){
+           showMessageDialog(
+                null, //janela pai
+                mensagem, //mensagem
+                NOME_APP, //titulo
+                JOptionPane.PLAIN_MESSAGE
+           );
+       }//exibir
+    
+}//class
